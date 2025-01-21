8364ac2e293f2a185857e2f7b97465ff65d11ceb
jundayw <jundayw@126.com>
2025-01-21 22:35:41 +0800

    Fallback

--- src/Attributes/Fallback.php
+++ src/Attributes/Fallback.php
@@ -2,11 +2,52 @@
 
 namespace Annotation\Routing\Attributes;
 
+use Annotation\Routing\Contracts\InvokeContract;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_CLASS)]
-class Fallback extends Route
+class Fallback extends Route implements Contracts\RouteFallbackAttributeContract
 {
+    public function __construct(
+        string|null                $name = null,
+        string|null                $action = null,
+        string|null                $controller = null,
+        InvokeContract|string|null $uses = null,
+        string|null                $prefix = null,
+        string|null                $domain = null,
+        array                      $middleware = [],
+        array                      $withoutMiddleware = [],
+        array                      $defaults = [],
+        array                      $bindingFields = [],
+        int|null                   $lockSeconds = null,
+        int|null                   $waitSeconds = null,
+        bool                       $withTrashed = false,
+        string                     $version = '1.0.0'
+    )
+    {
+        parent::__construct(
+            methods: self::GET,
+            name: $name,
+            uri: '{fallbackPlaceholder}',
+            action: $action,
+            controller: $controller,
+            uses: $uses,
+            prefix: $prefix,
+            domain: $domain,
+            middleware: $middleware,
+            withoutMiddleware: $withoutMiddleware,
+            defaults: $defaults,
+            wheres: [
+                'fallbackPlaceholder' => '.*',
+            ],
+            bindingFields: $bindingFields,
+            lockSeconds: $lockSeconds,
+            waitSeconds: $waitSeconds,
+            withTrashed: $withTrashed,
+            version: $version
+        );
+    }
+
     protected function boot(): void
     {
         $this->fallback = true;
