e9b9a5abcbfc5e0345e003931139315337b2f7d9
jundayw <jundayw@126.com>
2025-01-22 16:49:23 +0800

    Route::$namespace

--- src/Attributes/Route.php
+++ src/Attributes/Route.php
@@ -2,32 +2,33 @@
 
 namespace Annotation\Routing\Attributes;
 
+use Annotation\Routing\Contracts\InvokeContract;
 use Attribute;
-use Closure;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 class Route implements Contracts\RouteAttributeContract
 {
-    protected bool  $fallback = false;
+    protected bool $fallback = false;
 
     public function __construct(
-        protected array               $methods = [],
-        protected string|null         $name = null,
-        protected string|null         $uri = null,
-        protected string|null         $action = null,
-        protected string|null         $controller = null,
-        protected string|null|Closure $uses = null,
-        protected string|null         $prefix = null,
-        protected string|null         $domain = null,
-        protected array               $middleware = [],
-        protected array               $withoutMiddleware = [],
-        protected array               $defaults = [],
-        protected array               $wheres = [],
-        protected array               $bindingFields = [],
-        protected int|null            $lockSeconds = null,
-        protected int|null            $waitSeconds = null,
-        protected bool                $withTrashed = false,
-        protected string              $version = '1.0.0',
+        protected array                      $methods = [],
+        protected string|null                $name = null,
+        protected string|null                $uri = null,
+        protected string|null                $action = null,
+        protected string|null                $controller = null,
+        protected string|null                $namespace = null,
+        protected string|null|InvokeContract $uses = null,
+        protected string|null                $prefix = null,
+        protected string|null                $domain = null,
+        protected array                      $middleware = [],
+        protected array                      $withoutMiddleware = [],
+        protected array                      $defaults = [],
+        protected array                      $wheres = [],
+        protected array                      $bindingFields = [],
+        protected int|null                   $lockSeconds = null,
+        protected int|null                   $waitSeconds = null,
+        protected bool                       $withTrashed = false,
+        protected string                     $version = '1.0.0',
     )
     {
         $this->boot();
