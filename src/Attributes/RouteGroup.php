5c2ee1de50b6402ec7bd3356e14efaebcfc5fa8f
jundayw <jundayw@126.com>
2025-01-28 00:57:56 +0800

    wip:RouteGroup

--- src/Attributes/RouteGroup.php
+++ src/Attributes/RouteGroup.php
@@ -2,10 +2,49 @@
 
 namespace Annotation\Routing\Attributes;
 
+use Annotation\Routing\Contracts\InvokeContract;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_CLASS)]
 class RouteGroup extends Route
 {
+    public function __construct(
+        array                      $methods = [],
+        ?string                    $name = null,
+        ?string                    $controller = null,
+        ?string                    $namespace = null,
+        InvokeContract|string|null $uses = null,
+        ?string                    $prefix = null,
+        ?string                    $domain = null,
+        array                      $middleware = [],
+        array                      $withoutMiddleware = [],
+        array                      $defaults = [],
+        array                      $wheres = [],
+        array                      $bindingFields = [],
+        ?int                       $lockSeconds = null,
+        ?int                       $waitSeconds = null,
+        bool                       $withTrashed = false,
+        string                     $version = '1.0.0'
+    )
+    {
+        parent::__construct(
+            methods: $methods,
+            name: $name,
+            controller: $controller,
+            namespace: $namespace,
+            uses: $uses,
+            prefix: $prefix,
+            domain: $domain,
+            middleware: $middleware,
+            withoutMiddleware: $withoutMiddleware,
+            defaults: $defaults,
+            wheres: $wheres,
+            bindingFields: $bindingFields,
+            lockSeconds: $lockSeconds,
+            waitSeconds: $waitSeconds,
+            withTrashed: $withTrashed,
+            version: $version
+        );
+    }
 
 }
