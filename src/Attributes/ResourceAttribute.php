76b688744329f4dd5ee5637ac2d04aa182458faa
jundayw <jundayw@126.com>
2025-01-28 00:37:34 +0800

    wip

--- src/Attributes/ResourceAttribute.php
+++ src/Attributes/ResourceAttribute.php
@@ -4,15 +4,15 @@ namespace Annotation\Routing\Attributes;
 
 use Annotation\Routing\Contracts\InvokeContract;
 
-abstract class ResourceAttribute extends Route implements Contracts\RouteResourceAttributeContract
+class ResourceAttribute extends Route implements Contracts\RouteResourceAttributeContract
 {
     protected array $resource = [];
 
     public function __construct(
+        string|null                $name = null,
         protected array|string     $only = [],
         protected array|string     $except = [],
         protected array            $names = [],
-        string|null                $uri = null,
         string|null                $action = null,
         string|null                $controller = null,
         string|null                $namespace = null,
@@ -33,7 +33,7 @@ abstract class ResourceAttribute extends Route implements Contracts\RouteResourc
         $this->only   = is_array($this->only) ? $this->only : [$this->only];
         $this->except = is_array($this->except) ? $this->except : [$this->except];
         parent::__construct(
-            uri: $uri,
+            name: $name,
             action: $action,
             controller: $controller,
             namespace: $namespace,
@@ -64,14 +64,51 @@ abstract class ResourceAttribute extends Route implements Contracts\RouteResourc
     }
 
     /**
+     * @param string|null $url
      * @param string|null $name
      *
      * @return string|null
      */
     #[\Override]
-    public function getName(string $name = null): ?string
+    public function getName(string $url = null, string $name = null): ?string
     {
-        return $this->names[$name] ?? $name;
+        return array_key_exists($name, $this->names) ? $this->names[$name] : ($this->name ?: $url) . '.' . $name;
+    }
+
+    /**
+     * @param string|null $url
+     * @param string|null $method
+     *
+     * @return string|null
+     */
+    #[\Override]
+    public function getUri(string $url = null, string $method = null): ?string
+    {
+        $methods = explode('.', array_key_exists($method, $this->names) ? $this->names[$method] : ($this->name ?? $url));
+        $uri     = $this->getNestedResourceUri($methods);
+        $last    = end($methods);
+        return match ($method) {
+            'index', 'store' => str_replace("/{{$last}}", '', $uri),
+            'create' => str_replace("/{{$last}}", '/create', $uri),
+            'show', 'update', 'destroy' => $uri,
+            'edit' => "{$uri}/edit",
+            default => null
+        };
+    }
+
+    protected function getNestedResourceUri(array $segments): string
+    {
+        // We will spin through the segments and create a place-holder for each of the
+        // resource segments, as well as the resource itself. Then we should get an
+        // entire string for the resource URI that contains all nested resources.
+        return implode('/', array_map(function ($s) {
+            return "{$s}/{{$s}}";
+        }, $segments));
+    }
+
+    public function getNames(): array
+    {
+        return $this->names;
     }
 
     public function getResourceMethods(): array
